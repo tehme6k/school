7d78b7a17551d85ee6e6ca75ab75d237d7b79452
tim <moshimoshi6k@gmail.com>
2021-03-22 11:35:46 -0500

    init layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,8 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->name('dashboard');
+
+Route::get('admin/logout', [AdminController::class, 'Logout'])->name('admin.logout');
+
